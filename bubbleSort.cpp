342e552cbf5f27e3d276f4b6800932af9c65e019
ppero <pperokekw@gmail.com>
2025-09-11 10:51:06 -0300

    bubbleSort

---
+++ bubbleSort.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+void printArray(int arr[], int len){
+  for (int i = 0; i < len; i++){
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+}
+
+void bubbleSort(int arr[], int len){
+  int i, temp, k = 0, trocou = 1, m = len-1; 
+  while(trocou){
+    trocou = 0;
+    for(i = 0; i < m; i++){ //cada varredura
+      if(arr[i] > arr[i+1]){
+        temp = arr[i];
+        arr[i] = arr[i+1];
+        arr[i+1] = temp;
+        trocou = 1;
+        k = i ;
+      }
+    }
+    m = k;
+  }
+}
+
+int main(){
+  int array[10];
+  random_device rd;
+  mt19937 gen(rd());
+
+  for(int i = 0; i <10; i++){
+    uniform_int_distribution<> distr(0,30);
+    int random_number = distr(gen);
+    array[i] = random_number;
+  }
+
+  printArray(array,10);
+  bubbleSort(array,10);
+  printArray(array,10);
+
+  return 0;
+
+
+}
--- insertionSort.cpp
+++ insertionSort.cpp
@@ -2,7 +2,7 @@
 #include <random>
 using namespace std;
 
-void insertionSort(int c[], int len){
+vdasdasoid insertionSort(int c[], int len){
   int i = 0, j = 0, chave = 0;
   for(j =1; j < len; j++){ // from j = 1 to n-1 
     chave = c[j]; // seleciona o elemento do array ordenado
@@ -18,18 +18,18 @@ void insertionSort(int c[], int len){
 
 void printArray(int arr[], int len){
   for(int i =0; i< len; i++){
-    std::cout << arr[i] << " ";
+    cout << arr[i] << " ";
   }
   cout << endl;
 }
 int main(){
   int array[10];
-  std::random_device rd;
-  std::mt19937 gen(rd());
+  random_device rd;
+  mt19937 gen(rd());
   
 
   for(int i = 0; i<10; i++){
-    std::uniform_int_distribution<> distr(0,30);
+    uniform_int_distribution<> distr(0,30);
     int random_number = distr(gen);
     array[i] = random_number;
   }
